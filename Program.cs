0ea0c1b40036849a8ab8d5755c7a76f739812672
Bright Chidozie Onyekachi <64733058+Brightajuzie@users.noreply.github.com>
2024-09-28 12:38:08 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("What is your first name? ");
+string name = Console.ReadLine();
+
+Console.Write("What is your last name?");
+string lastname = Console.ReadLine();
+Console.WriteLine($"Your name is {name}, {lastname}");
